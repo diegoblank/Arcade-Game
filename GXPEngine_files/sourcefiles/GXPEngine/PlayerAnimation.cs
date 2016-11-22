c588d33cce648228895d810944c9215d02563482
diegoblank <diegobrunoblank@gmail.com>
2016-11-22 15:10:38 +0100

    staged

---
+++ GXPEngine_files/sourcefiles/GXPEngine/PlayerAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace GXPEngine
+{
+	public class PlayerAnimation : AnimationSprite
+	{
+
+		private int _timer;
+		private int _frameTimer;
+
+		public PlayerAnimation() : base ("playeranimation.png", 6, 4, 15)
+		{
+
+			SetOrigin(width / 2, height);
+			_timer = 20;
+			_frameTimer = 3;
+
+			SetScaleXY(6.0f, 6.0f);
+
+		}
+
+		void Update()
+		{
+
+			if (_frameTimer <= 0)
+			{
+				NextFrame();
+				_frameTimer = 2;
+
+			}
+
+
+			_timer = _timer - 1;
+			_frameTimer = _frameTimer - 1;
+
+			if (_timer <= 0)
+			{
+				_timer = 0;
+			}
+
+		}
+
+	}
+}
