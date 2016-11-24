afa6710f04811e89f5d8c7c1abc13f20ef0ae221
Rouge\DiegoBlank <diegobrunoblank@gmail.com>
2016-11-25 00:32:40 +0100

    no message

--- GXPEngine_files/sourcefiles/GXPEngine/Enemy.cs
+++ GXPEngine_files/sourcefiles/GXPEngine/Enemy.cs
@@ -45,7 +45,12 @@ namespace GXPEngine
 
 		void Update() 
 		{
+			if ((this.x - Player.PlayerPosX) < 30) 
+			{
+				enemyAnim1.Enemy1AnimState = 3;
 			
+			}
+
 			if (this.x < Player.PlayerPosX) 
 			{
 				x = x + _walkSpeed;
---
+++ GXPEngine_files/sourcefiles/GXPEngine/HealthPickup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GXPEngine
+{
+	public class HealthPickup : Sprite 
+	{
+		public HealthPickup() : base ("healthpickup.png")
+		{
+			SetOrigin(width / 2, height / 2);
+
+		}
+	}
+}
