b3f6af3d7105a3edcaab33f530ef8feca444dc48
diegoblank <diegobrunoblank@gmail.com>
2016-11-24 16:19:32 +0100

    FIXED DA ANIMATIONS

--- GXPEngine_files/sourcefiles/GXPEngine/PlayerAnimation.cs
+++ GXPEngine_files/sourcefiles/GXPEngine/PlayerAnimation.cs
@@ -21,33 +21,69 @@ namespace GXPEngine
 		void Update()
 		{
 
-			step = step + 1;
+			step = step - 1;
 
-			if (step > 2 && AnimState == 1)
+			if (step <= 0) 
 			{
-				NextFrame();
 				step = 0;
+			}
+
+			if (step <= 1  && AnimState == 1)
+			{
+
 				Mirror(false, false);
 
 			}
 
-			if (step > 2 && AnimState == 2)
+			if (step <= 1  && AnimState == 2)
 			{
-				NextFrame();
-				step = 0;
+				
 				Mirror(true, false);
 
 			}
 
-			if (step > 2 && AnimState == 0)
+
+
+
+			switch (AnimState)
 			{
-				
-				NextFrame();
-				step = 0;
+				case 0:
+					if (currentFrame < 19 && step <= 0) {currentFrame++;} 
+					else currentFrame = 15;
+					break;
+				case 1:
+					if (currentFrame < 15 && step <= 0) { currentFrame++;}
+					else currentFrame = 0;
+					break;
+				case 2:
+					if (currentFrame < 15 && step <= 0) { currentFrame++;}
+					else currentFrame = 0;
+					break;
+				default:
+					break;
 			}
 
 	
 	
+
+		}
+
+		public void switchAnimState(int state)
+		{
+			switch (state)
+			{
+				case 0:
+					currentFrame = 15;
+					break;
+				case 1:
+					currentFrame = 0;
+					break;
+				case 2:
+					currentFrame = 0;
+					break;
+				default:
+					break;
+			}
 
 		}
 
